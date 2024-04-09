4714bdb3f0ad4cfd543ea21dad1b2322f831fcd3
Ronan O'Brien-Hall <135774404+RonanOBHMassey@users.noreply.github.com>
2024-04-10 10:36:26 +1200

    Instructions and Working Rounds

--- B_01_HL_Game_V1.py
+++ B_01_HL_Game_V1.py
@@ -17,20 +17,15 @@ def instructions():
     print('''
 **** INSTRUCTIONS ****
 
-To begin, decide on a score goal
-(eg: the first one to reach a score of 50 wins).
+To begin, Choose the number of rounds and either
+customize the game parameters or go with the default game
+(where the secret number is between 1 - 100).
 
-For each round of the game, you win points by rolling dice.
-The winner of the round is the one who gets 13 (or slightly less)
+Then choose the number of rounds you'd like to play,
+Press <enter> for Infinite mode.
 
-If you win the round, then your score will increase by the number of points you earned.
-If your first roll of two dice is a double (eg: both dice are the same number) 
-then your score will be DOUBLE the number of points
-
-If you lose the round, say goodbye to those potential points!
-
-If you and the computer tie, (eg: you both get a score of 11) 
-then you will get the points and the computer will not.
+Your goal is to guess the secret number 
+without running out of guesses.
 
 Good Luck, Player!
     ''')
